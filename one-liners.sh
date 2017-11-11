5c45cc123b2f20d39dbdbf8415198a6028b58660
Henrik Karlsson <ineentho@users.noreply.github.com>
2017-11-11 13:49:20 +0100

    Update one-liners.sh

--- one-liners.sh
+++ one-liners.sh
@@ -1,4 +1,7 @@
 ### Virtualbox
 
-# Delete all VMS:
+# Delete all VMs:
 vboxmanage list vms | cut -d' ' -f1 | sed -E 's/"//g' | xargs -n1 vboxmanage unregistervm --delete
+
+# Force shutdown all VMs
+vboxmanage list runningvms |cut -f1 -d' ' | sed 's/"//g' | xargs -n1 -I{} vboxmanage controlvm "{}" poweroff
